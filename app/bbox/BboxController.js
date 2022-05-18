34c1e613ceb617f1d07f7eca777d7106327b42dd
Julian Mejliker <julian.mejliker@securitize.io>
2022-05-18 16:41:16 +0200

    Added links for apps

--- app/bbox/BboxController.js
+++ app/bbox/BboxController.js
@@ -3,13 +3,21 @@ import axios from 'axios';
 export default class BboxControler {
     bBoxOne(req, res) {
         //TODO: llamar al servicio de bbox puerto 9000
-        axios.get('http://127.0.0.1:9090/')
-      res.send("bbox-one");
+        axios.get('http://bbox:9090/').then(response => {
+            res.send(response.data);
+        }).catch(error => { 
+            res.send(error);
+        });
+    //   res.send("bbox-one");
     }
 
     bBoxTwo(req, res) {
         //TODO: llamar al servicio de bbox puerto 9001
-        axios.get('http://127.0.0.1:9091/')
-        res.send("bbox-two");
+        axios.get('http://bbox:9091/').then(response => {
+            res.send(response.data);
+        }).catch(error => {
+            res.send(error);
+        });
+        // res.send("bbox-two");
     }
 }
