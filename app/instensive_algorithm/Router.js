f9f6ca48f3acdc7d206ee5398d0bf8a8535d37f3
matiaseiglesias <matiaseiglesias@yahoo.com>
2022-05-18 23:53:11 -0300

    se corrigen los test de carga

--- app/instensive_algorithm/Router.js
+++ app/instensive_algorithm/Router.js
@@ -5,7 +5,7 @@ const router = Router();
 
 const intensiveAlgorithmController = new IntensiveAlgorithmController();
 
-router.get('/intensive', (req, res) => {
+router.get('/node/intensive', (req, res) => {
     intensiveAlgorithmController.getCalculation(req, res)
 });
 
